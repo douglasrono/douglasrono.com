9d42e3711723fc1aa66322a0c1acb64f83c6ccff
douglasrono <douglasronno@gmail.com>
2024-07-14 10:36:32 +0300

    "Updated app.blade.php layout file: changed HTML lang attribute, updated title, meta tags, and favicons, added schema.org JSON-LD, and reformatted code."

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,24 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-     <title>Senior Software Developer.</title>
-    <meta content="Experienced software developer, I take pride in delivering robust and reliable software solutions that meet my clients'
-     business goals." name="description">
-    <meta content="software developer, laravel developer, github, vue developer, MERN Stack developer" name="keywords">
+    <title>Douglas Rono - Senior Software Developer | PHP, JavaScript, Vue, Laravel</title>
+    <meta name="description"
+        content="Douglas Rono, an experienced senior software developer skilled in PHP, JavaScript, Vue, and Laravel. Providing robust and reliable software solutions to meet business goals.">
+    <meta name="keywords"
+        content="Douglas Rono, software developer, PHP, JavaScript, Vue, Laravel, web development, MERN stack developer, senior software developer">
 
-    <!-- Favicons -->
     <link href="assets/img/portfolio/port3.jpeg" rel="icon">
     <link href="assets/img/portfolio/port3.jpeg" rel="apple-touch-icon">
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
     <link href="assets/vendor/aos/aos.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -27,25 +26,37 @@
     <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
 
-    <!-- Scripts -->
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "name": "Douglas Rono",
+      "jobTitle": "Senior Software Developer",
+      "description": "Experienced software developer skilled in PHP, JavaScript, Vue, and Laravel.",
+      "url": "https://www.douglasrono.com",
+      "sameAs": [
+        "https://www.linkedin.com/in/douglasrono",
+        "https://github.com/douglasrono"
+      ]
+    }
+    </script>
+
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body>
 
+<body>
     <div id="app">
-        <header-component></header-component>
         <main class="py-4">
+
             @yield('content')
         </main>
     </div>
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
     <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
     <script src="assets/vendor/aos/aos.js"></script>
     <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -53,8 +64,7 @@
     <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
     <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
     <script src="assets/vendor/php-email-form/validate.js"></script>
-
-    <!-- Template Main JS File -->
     <script src="assets/js/main.js"></script>
 </body>
+
 </html>
