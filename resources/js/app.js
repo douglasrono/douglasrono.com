04953bf5137bad6c4724618759f6c672810ca416
douglasrono <douglasronno@gmail.com>
2024-07-14 18:12:38 +0300

    "Remove HeaderComponent import and registration"

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,7 @@ import { createApp } from 'vue';
 
 const app = createApp({});
 
-import HeaderComponent from './components/HeaderComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
-app.component('header-component', HeaderComponent);
 app.component('footer-component', FooterComponent);
-
-
 
 app.mount('#app');
